fea246b1836cf97a28830a90fc58bc0f5512040d
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-01-07 16:59:14 -0600

    Added Model Validation to Users.

--- DatingApp.API/Dtos/UserForRegistrationDto.cs
+++ DatingApp.API/Dtos/UserForRegistrationDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegistrationDto
     {
+        [Required]
         public string UserName { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Your password must be between 4 and 8 characters.")]
         public string Password { get; set; }
     }
 }
