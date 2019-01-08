1d42dcce47e418b3f1b8a4470d08023f0f65c827
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-01-08 16:07:48 -0600

    Added basic validation to Login form.

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  public model: any = {};
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public login(): void {
+    console.log(this.model);
+  }
 }
