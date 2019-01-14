0b4159112a0d41be14b2077b7b04a50be0c0ac30
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-01-14 10:29:38 -0600

    Added Bootswatch and ngx-bootstrap.

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
+import { BsDropdownModule } from 'ngx-bootstrap';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -21,7 +23,8 @@ import { ErrorInterceptorProvider } from './services/error.interceptor';
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [ErrorInterceptorProvider],
    bootstrap: [
