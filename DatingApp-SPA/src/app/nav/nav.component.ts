91a7b34c9b22a155a3c6106a011ea8d0479fb08c
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-01-14 11:25:11 -0600

    Added basic routing and templates for future components.

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { AlertifyService } from '../services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,17 +11,25 @@ import { AlertifyService } from '../services/alertify.service';
 export class NavComponent implements OnInit {
   public model: any = {};
 
-  constructor(public authService: AuthService, private alertifyService: AlertifyService) { }
+  constructor(
+    public authService: AuthService,
+    private alertifyService: AlertifyService,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   public login(): void {
-    this.authService.login(this.model).subscribe(next => {
-      this.alertifyService.success('Logged in successfully');
-    }, error => {
-      this.alertifyService.error(error);
-    });
+    this.authService.login(this.model).subscribe(
+      next => {
+        this.alertifyService.success('Logged in successfully');
+      },
+      error => {
+        this.alertifyService.error(error);
+      }, () => {
+        this.router.navigate(['/members']);
+      }
+    );
   }
 
   public loggedIn(): boolean {
@@ -30,5 +39,6 @@ export class NavComponent implements OnInit {
   public logout(): void {
     localStorage.removeItem('token');
     this.alertifyService.message('Logged out');
+    this.router.navigate(['/home']);
   }
 }
