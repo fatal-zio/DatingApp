5b1c3b4e6d42dc63a519f213a554dba477601bb7
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-01-18 11:26:04 -0600

    Basic username and password validation.

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { BsDropdownModule, TabsModule } from 'ngx-bootstrap';
@@ -47,6 +47,7 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     NgxGalleryModule,
     FileUploadModule,
     BsDropdownModule.forRoot(),
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { AlertifyService } from '../services/alertify.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,18 +11,29 @@ import { AlertifyService } from '../services/alertify.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegistration = new EventEmitter();
   public model: any = {};
+  public registerForm: FormGroup;
 
   constructor(private authService: AuthService, private alertifyService: AlertifyService) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', Validators.required)
+    }, this.passwordMatchValidator);
+  }
+
+  public passwordMatchValidator(group: FormGroup) {
+    return group.get('password').value === group.get('confirmPassword').value ? null : {'mismatch': true};
   }
 
   public register(): void {
-    this.authService.register(this.model).subscribe(() => {
+    /* this.authService.register(this.model).subscribe(() => {
       this.alertifyService.success('Registration successful');
     }, error => {
       this.alertifyService.error(error);
-    });
+    }); */
+    console.log(this.registerForm.value);
   }
 
   public cancel(): void {
