eba7db0d4d50477d8b316f70d4a029bdce1e31ff
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-01-21 14:58:01 -0600

    Added Like entity.

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -13,5 +13,24 @@ namespace DatingApp.API.Data
         public DbSet<Value> Values { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<Like> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Like>()
+                .HasKey(o => new {o.LikerId, o.LikeeId});
+
+            builder.Entity<Like>()
+                .HasOne(o => o.Likee)
+                .WithMany(o => o.Likers)
+                .HasForeignKey(o => o.LikeeId)
+                .OnDelete(DeleteBehavior.Restrict);
+            
+            builder.Entity<Like>()
+                .HasOne(o => o.Liker)
+                .WithMany(o => o.Likees)
+                .HasForeignKey(o => o.LikerId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
