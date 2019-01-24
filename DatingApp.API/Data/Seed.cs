80206a421adbae39014a0b44c5f4dfd6ae1ebdd0
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-01-24 13:40:11 -0600

    Added Roles to Data Seeding code.

--- DatingApp.API/Data/Seed.cs
+++ DatingApp.API/Data/Seed.cs
@@ -10,8 +10,13 @@ namespace DatingApp.API.Data
     public class Seed
     {
         private readonly UserManager<User> _userManager;
+        private readonly RoleManager<Role> _roleManager;
 
-        public Seed(UserManager<User> userManager) => _userManager = userManager;
+        public Seed(UserManager<User> userManager, RoleManager<Role> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
 
         public void SeedUsers()
         {
@@ -23,9 +28,36 @@ namespace DatingApp.API.Data
             var userData = File.ReadAllText("Data/UserSeedData.json");
             var users = JsonConvert.DeserializeObject<List<User>>(userData);
 
+            var roles = new List<Role>
+            {
+                new Role { Name = "Member" },
+                new Role { Name = "Admin" },
+                new Role { Name = "Moderator" },
+                new Role { Name = "VIP" },
+            };
+            
+            foreach(var role in roles)
+            {
+                _roleManager.CreateAsync(role).Wait();
+            }
+
             foreach (var user in users)
             {
                 _userManager.CreateAsync(user, "password").Wait();
+                _userManager.AddToRoleAsync(user, "Member").Wait();
+            }
+
+            var adminUser = new User
+            {
+                UserName = "Admin"
+            };
+
+            var result = _userManager.CreateAsync(adminUser, "password").Result;
+
+            if (result.Succeeded)
+            {
+                var admin = _userManager.FindByNameAsync("Admin").Result;
+                _userManager.AddToRolesAsync(admin, new[] {"Admin", "Moderator"}).Wait();
             }
         }
 
