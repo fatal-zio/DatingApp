81c32fcf9f3ec0befe4e5a17eb9ddd6c9741e0fb
fatal-zio <40217128+fatal-zio@users.noreply.github.com>
2019-01-24 14:19:45 -0600

    Added UsersWithRoles Action to Admin Controller.

--- DatingApp.API/Controllers/AdminController.cs
+++ DatingApp.API/Controllers/AdminController.cs
@@ -1,15 +1,39 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    
     [ApiController]
     [Route("api/[controller]")]
     public class AdminController: ControllerBase
     {
+        private readonly DataContext _context;
+
+        public AdminController(DataContext context) => _context = context;
+
         [Authorize(Policy = "RequireAdminRole")]
         [HttpGet("usersWithRoles")]
-        public IActionResult GetUsersWithRoles() => Ok("Only Admins can see this.");
+        public async Task<IActionResult> GetUsersWithRoles()
+        {
+            var userList = await (from user in _context.Users orderby user.UserName
+                                    select new 
+                                    {
+                                        Id = user.Id,
+                                        UserName = user.UserName,
+                                        Roles = (from userRole in user.UserRoles
+                                                    join role in _context.Roles
+                                                    on userRole.RoleId 
+                                                    equals role.Id
+                                                    select role.Name).ToList()
+                                    }).ToListAsync();
+
+            return Ok(userList);
+        } 
 
         [Authorize(Policy = "ModeratePhotoRole")]
         [HttpGet("photosForModeration")]
